afe52dcc6f35648989f22ee38309c73c3f69a4bb
pomepome <punyoon03@gmail.com>
2015-11-28 19:02:15 +0900

    Beta1.0.1

--- src/main/java/pome/waila/bcaddon/BCTransportModule.java
+++ src/main/java/pome/waila/bcaddon/BCTransportModule.java
@@ -15,23 +15,23 @@ public class BCTransportModule
 		}
 		catch (ClassNotFoundException e)
 		{
-			WailaBCAddon.log.error("Couldn't find BuildCraftTransport Class... What's wrong?");
+			WailaAddonBC.log.error("Couldn't find BuildCraftTransport Class... What's wrong?");
 			return;
 		}
 		try
 		{
 			tileGenericPipe = Class.forName("buildcraft.transport.TileGenericPipe");
-			WailaBCAddon.log.info("TileGenericPipe class found! Registring HUD...");
+			WailaAddonBC.log.info("TileGenericPipe class found! Registring HUD...");
 		}
 		catch (ClassNotFoundException e)
 		{
-			WailaBCAddon.log.error("Couldn't find TileGenericPipe Class... What's wrong?");
+			WailaAddonBC.log.error("Couldn't find TileGenericPipe Class... What's wrong?");
 			return;
 		}
 		ModuleRegistrar registrar = ModuleRegistrar.instance();
 		registrar.registerStackProvider(new HUDProviderBCPipe(), tileGenericPipe);
 		registrar.registerHeadProvider(new HUDProviderBCPipe(), tileGenericPipe);
 		registrar.registerTailProvider(new HUDProviderBCPipe(), tileGenericPipe);
-		WailaBCAddon.log.info("Registered successful!");
+		WailaAddonBC.log.info("Registered successful!");
 	}
 }
--- src/main/java/pome/waila/bcaddon/WailaAddonBC.java
+++ src/main/java/pome/waila/bcaddon/WailaAddonBC.java
@@ -1,5 +1,7 @@
 package pome.waila.bcaddon;
 
+import java.io.File;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,28 +11,31 @@ import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.common.config.Configuration;
 
-@Mod(modid="WailaBCAddon",name="WailaAddonBC",version = "1",dependencies = "required-after:Waila;required-after:BuildCraft|Transport@[7.0.3,)")
-public class WailaBCAddon
+@Mod(modid="WailaAddonBC",name="WailaAddonBC",version = "1.0.1",dependencies = "required-after:Waila;required-after:BuildCraft|Transport@[7.0.3,)")
+public class WailaAddonBC
 {
 	public static boolean enabled;
+	
+	private File confFile;
 
-	public static Logger log = LogManager.getLogger("WailaBCAddon");
+	public static Logger log = LogManager.getLogger("WailaAddonBC");
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
-		config.load();
-		enabled = config.getBoolean("enabled", "general", true, "Is the mod enabled?");
-		config.save();
+		confFile = new File(event.getModConfigurationDirectory(),"WailaAddonBC.cfg");
 	}
 
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
+		Configuration config = new Configuration(confFile);
+		config.load();
+		enabled = config.getBoolean("enabled", "general", true, "Is the mod enabled?");
 		if(enabled)
 		{
 			BCTransportModule.register();
 		}
+		config.save();
 	}
 }
